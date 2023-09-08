902da9c7cf730b56148c3a051a64ee7dd23e2fc9
Adam Dickinson <adamgamesyt@gmail.com>
2023-09-08 21:22:43 +0200

    Project Creation

---
+++ src/main/java/destiny/destinyitemmanagerbe/DestinyItemManagerBeApplication.java
@@ -0,0 +1,13 @@
+package destiny.destinyitemmanagerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyItemManagerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestinyItemManagerBeApplication.class, args);
+	}
+
+}
